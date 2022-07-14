146e6b575872b29bf79b513cfc76676687667502
kengohwebdev <107210314+kengohwebdev@users.noreply.github.com>
2022-07-14 16:37:17 -0500

    Update Program.cs

--- Vacation/Program.cs
+++ Vacation/Program.cs
@@ -1,105 +1,242 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 
-namespace Vacation
+
+public class TripDetails
 {
-    public class Vacation
+
+    public static void Main(String[] args)
     {
 
-       
+        Console.WriteLine(Solution.solution(2014, "April", "May", "Wednesday") + " weeks");
 
-        private int[] daysPerMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-   
-        private String[] months = {"January", "February", "March", "April", "May",
-            "June", "July", "August", "September", "October", "November", "December"};
-    
-        private String[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
-
-        private bool isLeapYear(int Y)
-        {
-            return Y % 4 == 0;
-        }
-
-   
-        public int getDayIndex(String day)
-        {
-            for (int i = 0; i < days.Length; i += 1)
-            {
-                if (days[i].Equals(day))
-                    return i;
-            }
-            return -1;
-        }
-
-
-        public int getMonthIndex(String month)
-        {
-            for (int i = 0; i < months.Length; i += 1)
-            {
-                if (months[i].Equals(month))
-                    return i;
-            }
-            return -1;
-        }
-
- 
-        public int getDayOfGivenMonth(String firstDayOfJanuary, int monthIndex, int day)
-        {
-            int firstDayIndex = getDayIndex(firstDayOfJanuary);
-            int totalDays = 0;
-            for (int i = 0; i < monthIndex; i += 1)
-                totalDays += daysPerMonth[i];
-            return (firstDayIndex + (totalDays - 1) % 7 + day) % 7;
-        }
-
-    
-        public int vacation(int year, String begin_Month, String end_Month, String DayOne)
-        {
-      
-            if (isLeapYear(year))
-            {
-                daysPerMonth[1] = 29;
-            }
-
-
-            int startingMonthIndex = getMonthIndex(begin_Month);
- 
-            int endingMonthIndex = getMonthIndex(end_Month);
-
-
-            if (endingMonthIndex < startingMonthIndex)
-                return -1;
-
-            int totalDays = 0;
-            for (int i = startingMonthIndex; i <= endingMonthIndex; i += 1)
-            {
-                totalDays += daysPerMonth[i];
-            }
-
-
-            int firstDayOfTheMonth = getDayOfGivenMonth(DayOne, startingMonthIndex, 1);
-      
-            int nextMondayIndex = firstDayOfTheMonth + (7 - firstDayOfTheMonth) % 7;
-            
-
-            totalDays -= (nextMondayIndex - 1);
-
-            
-            return totalDays / 7;
-            
-        }
-
-        class Program
-        {
-            public static void Main(string[] args)
-            {
-                Vacation vacation = new Vacation();
-                Console.WriteLine(Vacation.vacation(2022, "August", "September", "Thursday"));
-            }
-        }
     }
+
 }
+
+class MyCalendar
+{
+
+    // To store numeric values
+
+    private int day, month, year;
+
+    private int dayOfYear; // it will be in range from 0 to 6 ( Signifying Sunday to Saturday
+
+    private static int[] numDays = {
+
+31,
+
+28,
+
+31,
+
+30,
+
+31,
+
+30,
+
+31,
+
+31,
+
+30,
+
+31,
+
+30,
+
+31
+
+};
+
+    public MyCalendar(int dayOfYear, int year)
+    {
+
+        day = 1;
+
+        month = 0; // January
+
+        this.year = year;
+
+        this.dayOfYear = dayOfYear;
+
+    }
+
+    // check if a year is leap year or not
+
+    private static bool isLeapYear(int year)
+    {
+
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+
+    }
+
+    private static int getDays(int month, int year)
+    {
+
+        if (isLeapYear(year) && month == 1)
+        { // Feb
+
+            return numDays[month] + 1;
+
+        }
+        else
+        {
+
+            return numDays[month];
+
+        }
+
+    }
+
+    public void advance()
+    {
+
+        dayOfYear = (dayOfYear + 1) % 7;
+
+        day++;
+
+        int maxDaysInMonth = getDays(month, year);
+
+        // if it was last day of month, then increment the month
+
+        if (day > maxDaysInMonth)
+        {
+
+            day = 1;
+
+            month++;
+
+            // if it was 31st december, we need to change year also
+
+            if (month > 12)
+            {
+
+                month = 0;
+
+                year++;
+
+            }
+
+        }
+
+    }
+
+    public int getMonthIndex()
+    {
+
+        return month;
+
+    }
+
+    public int getDayOfMonthIndex()
+    {
+
+        return dayOfYear;
+
+    }
+
+    public override String ToString()
+    {
+
+        return "MyCalendar [\nday: " + day + "\nmonth: " + month + "\nyear: " + year + "\ndayOfYear: " + dayOfYear +
+
+        "]";
+
+    }
+
+}
+
+class Solution
+{
+
+    private static String[] Months = {
+
+"January",
+
+"February",
+
+"March",
+
+"April",
+
+"May",
+
+"June",
+
+"July",
+
+"August",
+
+"September",
+
+"October",
+
+"November",
+
+"December"
+
+};
+
+    private static String[] days = {
+
+"Sunday",
+
+"Monday",
+
+"Tuesday",
+
+"Wednesday",
+
+"Thursday",
+
+"Friday",
+
+"Saturday"
+
+};
+
+    public static int solution(int year, String beginningMonth,
+
+    String endMonth, String startDayOfYear)
+    {
+
+        MyCalendar calendar = new MyCalendar(Array.IndexOf(days, startDayOfYear), year);
+
+        int startMonthIndex = Array.IndexOf(Months, beginningMonth);
+
+        int endMonthIndex = Array.IndexOf(Months, endMonth);
+
+        // Reach to the date on which journey will start
+
+        while ((calendar.getMonthIndex() != startMonthIndex) ||
+
+        (!String.Equals(days[calendar.getDayOfMonthIndex()].ToLower(), "monday")))
+        {
+
+            calendar.advance();
+
+        }
+
+        int numDaysSpent = 0;
+
+        while (calendar.getMonthIndex() <= endMonthIndex)
+        {
+
+            numDaysSpent++;
+
+            calendar.advance();
+
+        }
+
+        // We can divide the days spent by 7 to get weeks
+
+        return numDaysSpent / 7;
+
+    }
+
+}
+
+
